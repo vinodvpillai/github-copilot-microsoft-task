b3e7a0a54058c39119b03529faf83ccde5ffe5d4
Vinod Pillai <vinodthebest@gmail.com>
2024-08-17 04:30:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMembers() {
+  const members = [
+    {
+      name: 'John',
+      skills: ['HTML', 'CSS', 'JS']
+    },
+    {
+      name: 'Jane',
+      skills: ['HTML', 'CSS', 'JS', 'React']
+    },
+    {
+      name: 'Jack',
+      skills: ['HTML', 'CSS', 'JS', 'React', 'Node']
+    }
+  ];
+  return members;
+}
